49849d4804fab0193160cf4bfb60d9f44fd5d305
justinPalmer1 <63416273+justinPalmer1@users.noreply.github.com>
2025-08-19 20:29:12 -0400

    app started. added shared project

---
+++ MyPersonalSite.Shared/Models/Milestones.cs
@@ -0,0 +1,13 @@
+﻿
+namespace MyPersonalSite.Shared.Models
+{
+  
+
+    public class Milestone
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = "";
+        public DateTime Date { get; set; }
+    }
+
+}
